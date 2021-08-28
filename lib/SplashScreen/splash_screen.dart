ca02964fb62268c622c902d0c71898cd543bdfe3
Altaf Razzaque <altafc22@gmail.com>
2021-08-28 15:51:43 +0400

    small bug fixes

---
+++ lib/SplashScreen/splash_screen.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:quick_mart/welcome/welcome_screen.dart';
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTimer(){
+    Timer(Duration(seconds: 5), () async{
+      Route newRoute = MaterialPageRoute(builder:  (context)=> WelcomeScreen());
+      Navigator.pushReplacement(context, newRoute);
+
+    });
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+    child:Container(
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+            colors:[
+              Colors.purple,
+              Colors.deepPurple,
+            ],
+          begin: const FractionalOffset(0.0, 0.0),
+          end: const FractionalOffset(1.0 , 0.0),
+          stops: [0.0,1.0],
+          tileMode: TileMode.clamp
+              ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment:    MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('assets/images/logo.png',width: 200.0,) ,
+       ),
+            SizedBox(height: 20.0,),
+            Center(
+              child: Text(
+                "Sell, Purchase Or Exchange Your Old Items",
+                    style: TextStyle(
+                  fontSize: 14.0,
+                color: Colors.white,
+                fontFamily: "Lobster"
+              )
+              )
+            ),
+          ],
+        ),
+      ),
+    ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart/SplashScreen/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
